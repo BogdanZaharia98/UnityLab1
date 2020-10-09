e8e70619ddb1602ccb8641c4acc1a6f97db11da0
BogdanZaharia98 <48146977+BogdanZaharia98@users.noreply.github.com>
2020-10-09 14:00:02 +0300

    commit1

---
+++ New Unity Project (2)/Assets/Editor/Vuforia/ImageTargetTextures/testDatabase/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject cube; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube=GameObject.Find("myCube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube.transform.position+=new Vector3(0,- (float)0.001 ,0);
+    }
+}
